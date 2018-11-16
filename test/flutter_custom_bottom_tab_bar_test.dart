e395257904815f7831960f5426dac5b2054924b0
LiuC520 <674668211@qq.com>
2018-11-16 12:30:55 +0800

    修改测试文件

--- test/flutter_custom_bottom_tab_bar_test.dart
+++ test/flutter_custom_bottom_tab_bar_test.dart
@@ -3,11 +3,5 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_custom_bottom_tab_bar/flutter_custom_bottom_tab_bar.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
-  });
+  test('adds one to input values', () {});
 }
